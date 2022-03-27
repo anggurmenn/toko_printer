0072a9fa455f238bb58f26ff6bccc501c21a6a30
dzikri <54340470+anggurmenn@users.noreply.github.com>
2022-03-27 17:55:50 +0700

    Add files via upload

---
+++ toko_elektronik/admin/index.php
@@ -0,0 +1,29 @@
+<?php 
+
+session_start();
+
+require '../koneksi.php';
+
+if(!isset($_SESSION["username"])) {
+    echo "
+    <script type='text/javascript'>
+        alert('Silahkan login terlebih dahulu')
+        window.location = '../login/index.php';
+    </script>
+    ";
+}
+
+
+
+
+
+?>
+
+
+<?php include '../layout/sidebar.php' ?>
+
+<div class="main">
+    <h2>Selamat datang di PrinterKita </h2>
+     <h5>Semangat kerjanya ngademin!! bonus menanti</h5>
+       
+</div>
